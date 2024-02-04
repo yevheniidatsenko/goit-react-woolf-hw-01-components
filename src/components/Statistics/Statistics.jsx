051d8d1a457e332a045d3fda5e0aaee5cd063fa1
Yevhenii Datsenko <yevheniydatsenko@gmail.com>
2024-02-05 00:04:42 +0200

    Separation of getRandomColor

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,5 @@
 import styles from './Statistics.module.css';
-
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
+import getRandomColor from '../Statistics/getRandomColor';
 
 const Statistics = ({ title, stats }) => {
   return (
